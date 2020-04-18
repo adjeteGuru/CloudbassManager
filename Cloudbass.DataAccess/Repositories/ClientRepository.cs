eb612b1cddc9fb3d8233140ca1b8456c1349642c
adjeteGuru <justinadjev@hotmail.com>
2020-04-18 21:53:47 +0100

    feat: client interface implemented to allow query

---
+++ Cloudbass.DataAccess/Repositories/ClientRepository.cs
@@ -0,0 +1,28 @@
+﻿using Cloudbass.DataAccess.Repositories.Contracts;
+using Cloudbass.Database;
+using Cloudbass.Database.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Cloudbass.DataAccess.Repositories
+{
+    public class ClientRepository : IClientRepository
+    {
+        private CloudbassContext _db;
+        public ClientRepository(CloudbassContext db)
+        {
+            _db = db;
+        }
+        public Client GetClient(int id)
+        {
+            return _db.Clients.SingleOrDefault(x => x.Id == id);
+        }
+
+        public IEnumerable<Client> GetClients()
+        {
+            return _db.Clients.ToList();
+        }
+    }
+}
---
+++ Cloudbass.DataAccess/Repositories/Contracts/IClientRepository.cs
@@ -0,0 +1,13 @@
+﻿using Cloudbass.Database.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cloudbass.DataAccess.Repositories.Contracts
+{
+    public interface IClientRepository
+    {
+        IEnumerable<Client> GetClients();
+        Client GetClient(int id);
+    }
+}
